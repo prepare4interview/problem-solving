7a6d6c5cdce12f7867afc0edb6a793587af765ee
Prepare4Interview <prepare4interview@gmail.org>
2014-01-27 10:22:53 +0000

    - the program was incorrectly implementing square root instead of cubic root - added test and support for negative numbers

--- src/main/java/org/interviewproblems/l1binarysearch/P2CubicRoot.java
+++ src/main/java/org/interviewproblems/l1binarysearch/P2CubicRoot.java
@@ -8,18 +8,21 @@ package org.interviewproblems.l1binarysearch;
 public class P2CubicRoot {
 
    //post:    nr - epsilon <= result*result <= nr + epsilon
-   public float sqrt(float nr, float epsilon) {
+   public float cubicRoot(float nr, float epsilon) {
+      float signum = Math.signum(nr);
+      nr = Math.abs(nr);
+
       float lo, hi, result;
-      if (nr > 1) {
+      if (nr >= 1) {
          lo = 1; hi = nr; result = 1;
       } else {
          lo = nr; hi = 1; result = 0;
       }
       int loopCount = 0;
-      while (!((result * result >= nr - epsilon) && (result * result) <= nr + epsilon)) {
+      while (!((result * result * result >= nr - epsilon) && (result * result * result) <= nr + epsilon)) {
          loopCount++;
          float mid = (lo + hi) / 2;
-         if (mid * mid < nr) {
+         if (mid * mid * mid < nr) {
             lo = mid;
          } else {
             hi = mid;
@@ -27,6 +30,6 @@ public class P2CubicRoot {
          result = (lo + hi) / 2;
       }
       System.out.printf("epsilon = %s, loop count = %s \n", epsilon, loopCount);
-      return result;
+      return result * signum;
    }
 }
--- src/test/java/org/interviewproblems/l1binaryserarch/P2CubicRootTest.java
+++ src/test/java/org/interviewproblems/l1binaryserarch/P2CubicRootTest.java
@@ -10,39 +10,55 @@ import static org.testng.AssertJUnit.assertTrue;
 public class P2CubicRootTest {
    P2CubicRoot p2CubicRoot = new P2CubicRoot();
 
-   //sqrt(5) = 2.236067977
+   //cubeRoot(5) = 1.709975946676697
    public void basicTestGraterThanOne() {
-      final float sqrt5 = 2.236068f;
-      assertLimits(sqrt5, p2CubicRoot.sqrt(5f, 1f), 1f);
-      assertLimits(sqrt5, p2CubicRoot.sqrt(5f, 0.1f), 0.1f);
-      assertLimits(sqrt5, p2CubicRoot.sqrt(5f, 0.01f), 0.01f);
-      assertLimits(sqrt5, p2CubicRoot.sqrt(5f, 0.001f), 0.001f);
-      assertLimits(sqrt5, p2CubicRoot.sqrt(5f, 0.0001f), 0.0001f);
-      assertLimits(sqrt5, p2CubicRoot.sqrt(5f, 0.00001f), 0.00001f);
-      assertLimits(sqrt5, p2CubicRoot.sqrt(5f, 0.000001f), 0.000001f);
+      double pow = Math.pow(5d, (1d / 3d));
+      System.out.println("Math.pow(5, 1/3) = " + pow);
+      final float cubeRoot5 = 1.709975946676697f;
+      assertLimits(cubeRoot5, p2CubicRoot.cubicRoot(5f, 1f), 1f);
+      assertLimits(cubeRoot5, p2CubicRoot.cubicRoot(5f, 0.1f), 0.1f);
+      assertLimits(cubeRoot5, p2CubicRoot.cubicRoot(5f, 0.01f), 0.01f);
+      assertLimits(cubeRoot5, p2CubicRoot.cubicRoot(5f, 0.001f), 0.001f);
+      assertLimits(cubeRoot5, p2CubicRoot.cubicRoot(5f, 0.0001f), 0.0001f);
+      assertLimits(cubeRoot5, p2CubicRoot.cubicRoot(5f, 0.00001f), 0.00001f);
+      assertLimits(cubeRoot5, p2CubicRoot.cubicRoot(5f, 0.000001f), 0.000001f);
+   }
+
+   public void basicTestSmallerThanMinusOne() {
+      double pow = Math.pow(-5f, (1d / 3d));
+      System.out.println("Math.pow(-5, 1/3) = " + pow);
+      final float cubeRoot5 = -1.709975946676697f;
+      assertLimits(cubeRoot5, p2CubicRoot.cubicRoot(-5f, 1f), 1f);
+      assertLimits(cubeRoot5, p2CubicRoot.cubicRoot(-5f, 0.1f), 0.1f);
+      assertLimits(cubeRoot5, p2CubicRoot.cubicRoot(-5f, 0.01f), 0.01f);
+      assertLimits(cubeRoot5, p2CubicRoot.cubicRoot(-5f, 0.001f), 0.001f);
+      assertLimits(cubeRoot5, p2CubicRoot.cubicRoot(-5f, 0.0001f), 0.0001f);
+      assertLimits(cubeRoot5, p2CubicRoot.cubicRoot(-5f, 0.00001f), 0.00001f);
+      assertLimits(cubeRoot5, p2CubicRoot.cubicRoot(-5f, 0.000001f), 0.000001f);
    }
 
    public void basicTestWithOne() {
-      assertEquals(1f, p2CubicRoot.sqrt(1, 1f));
-      assertEquals(1f, p2CubicRoot.sqrt(1, .1f));
-      assertEquals(1f, p2CubicRoot.sqrt(1, .01f));
-      assertEquals(1f, p2CubicRoot.sqrt(1, .001f));
-      assertEquals(1f, p2CubicRoot.sqrt(1, .00001f));
+      assertEquals(1f, p2CubicRoot.cubicRoot(1, 1f));
+      assertEquals(1f, p2CubicRoot.cubicRoot(1, .1f));
+      assertEquals(1f, p2CubicRoot.cubicRoot(1, .01f));
+      assertEquals(1f, p2CubicRoot.cubicRoot(1, .001f));
+      assertEquals(1f, p2CubicRoot.cubicRoot(1, .00001f));
    }
 
-   //sqrt(0.9) = 0.9486832854847512
+   //cubeRoot(0.9) = 0.9486832854847512
    public void testSmallerThanOne() {
-      final float sqrtDot9 = 0.9486832854847512f;
-      assertLimits(sqrtDot9, p2CubicRoot.sqrt(0.9f, 0.1f), 0.1f);
-      assertLimits(sqrtDot9, p2CubicRoot.sqrt(0.9f, 0.01f), .01f);
-      assertLimits(sqrtDot9, p2CubicRoot.sqrt(0.9f, 0.001f), .001f);
-      assertLimits(sqrtDot9, p2CubicRoot.sqrt(0.9f, 0.0001f), .0001f);
-      assertLimits(sqrtDot9, p2CubicRoot.sqrt(0.9f, 0.00001f), .00001f);
+      System.out.println("Math.pow(0.9, 1/3) = " + Math.pow(0.9, 1d / 3d));
+      final float sqrtDot9 = 0.9654893846056297f;
+      assertLimits(sqrtDot9, p2CubicRoot.cubicRoot(0.9f, 0.1f), 0.1f);
+      assertLimits(sqrtDot9, p2CubicRoot.cubicRoot(0.9f, 0.01f), .01f);
+      assertLimits(sqrtDot9, p2CubicRoot.cubicRoot(0.9f, 0.001f), .001f);
+      assertLimits(sqrtDot9, p2CubicRoot.cubicRoot(0.9f, 0.0001f), .0001f);
+      assertLimits(sqrtDot9, p2CubicRoot.cubicRoot(0.9f, 0.00001f), .00001f);
    }
 
 
    private void assertLimits(float sqrt5, float result, float epsilon) {
-      System.out.printf("Expected result = %s, actual result = %s \n", sqrt5, result);
+      System.out.printf("Expected result = %s, actual result = %s, epsilon = %s \n", sqrt5, result, epsilon);
       assertTrue(result < sqrt5 + epsilon);
       assertTrue(result > sqrt5 - epsilon);
    }
