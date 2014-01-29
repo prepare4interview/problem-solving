e1529714f5d2dd77ea9fed4564a7d8592c679399
Prepare4Interview <prepare4interview@gmail.org>
2014-01-29 10:43:22 +0000

    P7SortUnion - implement the search algorithm in (log(N))^2

--- src/main/java/org/interviewproblems/l1binarysearch/P7SortUnion.java
+++ src/main/java/org/interviewproblems/l1binarysearch/P7SortUnion.java
@@ -28,4 +28,57 @@ public class P7SortUnion {
       }
       return current;
    }
+
+   /**
+    * this executes in (lonN) ^ 2.
+    *
+    * @param k - a zero based index of the element in the reunion
+    */
+   public int findKLogNSquare(int[] a, int[] b, int k) {
+
+      if (k == 0) {
+         return Math.min(a[0], b[0]);
+      }
+
+      int sizeA = Math.min(a.length, k + 1);
+      int lo = -1, hi = sizeA;
+      // x in a, pos(x,b) + index(x,a) == k + 1 <=> x is the result
+      //invariant pos[lo,b] + lo < k - 1
+      while (hi - lo > 1) {
+         int mid = (lo + hi) / 2;
+         int pos = position(b, a[mid], k);
+         if (pos + mid < k) {
+            lo = mid;
+         } else {
+            hi = mid;
+         }
+      }
+
+      if (hi == sizeA) { //all elements in A are in the reunion
+         return b[k - hi];
+      }
+
+      int pos = position(b, a[hi], k);
+      if (pos + hi == k)
+         return a[hi];
+
+      if (pos + hi > k)
+         return b[pos - 1];
+
+      throw new IllegalStateException();
+   }
+
+   public int position(int[] b, int e, int k) {
+      int lo = -1, hi = Math.min(b.length, k + 1);
+      //invariant b[lo] < e
+      while (hi - lo > 1) {
+         int mid = (lo + hi) / 2;
+         if (b[mid] <= e) {
+            lo = mid;
+         } else {
+            hi = mid;
+         }
+      }
+      return hi;
+   }
 }
--- src/test/java/org/interviewproblems/l1binaryserarch/P7SortUnionTest.java
+++ src/test/java/org/interviewproblems/l1binaryserarch/P7SortUnionTest.java
@@ -26,4 +26,57 @@ public class P7SortUnionTest {
          assertEquals(i, sortUnion.findK(second, first, i));
       }
    }
+
+   public void testSortLogNSquareComplex() {
+      int[] first = new int[]{1, 3, 5, 7, 9, 10, 11, 12, 13, 14, 15};
+      int[] second = new int[]{0, 2, 4, 6, 8};
+
+      assertEquals(9, sortUnion.findKLogNSquare(second, first, 9));
+
+      for (int i = 0; i < 16; i++) {
+         assertEquals(i, sortUnion.findKLogNSquare(first, second, i));
+         assertEquals(i, sortUnion.findKLogNSquare(second, first, i));
+      }
+   }
+
+
+   public void testSortLogNSquare() {
+      int[] first = new int[]{1, 3, 5, 7, 9};
+      int[] second = new int[]{0, 2, 4, 6, 8};
+      assertEquals(4, sortUnion.findKLogNSquare(second, first, 4));
+      assertEquals(4, sortUnion.findKLogNSquare(first, second, 4));
+   }
+
+   public void testSortLogNSquareSameNumbers() {
+      int[] first = new int[]{1, 1, 1, 1, 1};
+      int[] second = new int[]{0, 0, 0, 0, 0};
+      assertEquals(0, sortUnion.findKLogNSquare(second, first, 4));
+      assertEquals(0, sortUnion.findKLogNSquare(first, second, 4));
+   }
+
+   public void testSortLogNSquareSameArray() {
+      int[] first = new int[]{0, 1, 2, 3, 4};
+      int[] second = new int[]{0, 1, 2, 3, 4};
+      assertEquals(2, sortUnion.findKLogNSquare(second, first, 4));
+      assertEquals(2, sortUnion.findKLogNSquare(first, second, 4));
+   }
+
+   public void testSortLogNSquareDisjunct() {
+      int[] first = new int[]{5, 6, 7, 8, 9};
+      int[] second = new int[]{0, 1, 2, 3, 4};
+      assertEquals(4, sortUnion.findKLogNSquare(first, second, 4));
+      assertEquals(4, sortUnion.findKLogNSquare(second, first, 4));
+   }
+
+   public void testPosition() {
+      int[] b = new int[]{1, 3, 5, 7, 9};
+      assertEquals(0, sortUnion.position(b, 0, b.length));
+      assertEquals(1, sortUnion.position(b, 2, b.length));
+      assertEquals(2, sortUnion.position(b, 4, b.length));
+      assertEquals(3, sortUnion.position(b, 6, b.length));
+      assertEquals(4, sortUnion.position(b, 8, b.length));
+
+      assertEquals(1, sortUnion.position(b, 1, b.length));
+      assertEquals(5, sortUnion.position(b, 9, b.length));
+   }
 }
