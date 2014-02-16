4fe4e9eede2bfe63778d63c21fe541ec2ad83060
Prepare4Interview <prepare4interview@gmail.org>
2014-02-16 12:42:24 +0000

    Sun monring

--- src/main/java/org/interviewproblems/l4recurssion/EditDistance.java
+++ src/main/java/org/interviewproblems/l4recurssion/EditDistance.java
@@ -12,37 +12,8 @@ public class EditDistance {
       }
    }
 
-   public int minDistance(String word1, String word2, int currentCost) {
-      if (word1.length() < word2.length()) {
-         int min = Integer.MAX_VALUE;
-         for (int i = 0; i < word1.length(); i++) {
-            String newWord1 = word1.substring(0, i) + word2.charAt(i) + word1.substring(i);
-            min = Math.min(min, minDistance(newWord1, word2, currentCost + 1));
-         }
-         return min;
-      }
-      if (word1.length() == word2.length()) {
-         for (int i = 0; i < word1.length(); i++) {
-            if (word1.charAt(i) != word2.charAt(i)) currentCost++;
-         }
-         return currentCost;
-      }
-      int min = Integer.MAX_VALUE;
-      for (int i = 0; i < word1.length(); i++) {
-         String newWord1 = word1.substring(0, i) + word1.substring(i + 1);
-         min = Math.min(min, minDistance(newWord1, word2, currentCost + 1));
-      }
-      return min;
-   }
-
-   HashMap<String, Integer> cache = new HashMap<String, Integer>();
-
    public int minDistance2(String word1, String word2) {
       if (word1.length() == 0 || word2.length() == 0) return word1.length() + word2.length();
-
-      if (cache.containsKey(word1 + "_" + word2)) {
-         return cache.get(word1 + "_" + word2);
-      }
 
       int minDist = Integer.MAX_VALUE;
 
@@ -58,31 +29,28 @@ public class EditDistance {
             continue;
          }
 
-         int posInWord2 = word2.indexOf(matchingSeq);
-         String w1 = word1.substring(0, i);
-         String w2 = word2.substring(0, posInWord2);
-         int md1 = minDistance2(w1, w2);
-         cache.put(w1 + "_" + w2, md1);
+         int posInWord2 = -1;
 
-         w1 = word1.substring(i + sequenceSize);
-         w2 = word2.substring(posInWord2 + sequenceSize);
-         int md2 = minDistance2(w1, w2);
-         cache.put(w1 + "_" + w2, md2);
-         minDist = Math.min(minDist, md1 + md2);
+         while ((posInWord2 = word2.indexOf(matchingSeq, posInWord2 + 1)) >= 0) {
+            String w1 = word1.substring(0, i);
+            String w2 = word2.substring(0, posInWord2);
+            int md1 = minDistance(w1, w2);
+
+            w1 = word1.substring(i + sequenceSize);
+            w2 = word2.substring(posInWord2 + sequenceSize);
+            int md2 = minDistance(w1, w2);
+            minDist = Math.min(minDist, md1 + md2);
+         }
          i += sequenceSize;
       }
 
-      if (minDist == Integer.MAX_VALUE) {
-         return word2.length();
-      }
-
-      return minDist;
+      return Math.min(minDist, word2.length());
    }
 
    public static void main(String[] args) {
       long start = System.currentTimeMillis();
-      String w1 = "ab";
-      String w2 = "bc";
+      String w1 = "industry"; //dustry
+      String w2 = "interest"; //terest
       int i = new EditDistance().minDistance(w1, w2);
       System.out.println("It took:" + (System.currentTimeMillis() - start));
       System.out.println("i = " + i);
---
+++ src/main/java/org/interviewproblems/l4recurssion/LruCache.java
@@ -0,0 +1,92 @@
+package org.interviewproblems.l4recurssion;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LruCache {
+
+   Node head;
+   Node tail;
+   final int capacity;
+
+   Map<Integer, Node> cache = new HashMap<Integer, Node>();
+
+   class Node {
+      Node prev;
+      Node next;
+      int value;
+      int key;
+      //todo add equals & hashcode
+   }
+
+   public LruCache(int capacity) {
+      this.capacity = capacity;
+   }
+
+   public int get(int key) {
+      Node n = cache.get(key);
+      if (n == null) return -1;
+      enqueue(n);
+      return n.value;
+   }
+
+   private void enqueue(Node n) {
+      if (n != tail) {
+         if (n.prev != null) {
+            n.prev.next = n.next;
+         } else {
+            head = n.next;
+         }
+         if (n.next != null) {
+            n.next.prev = n.prev;
+         }
+         n.prev = tail;
+         n.prev.next = n;
+         n.next = null;
+         tail = n;
+      }
+   }
+
+   public void set(int key, int value) {
+      Node n = cache.get(key);
+      if (n != null) {
+         n.value = value;
+         enqueue(n);
+      } else {
+         n = new Node();
+         n.value = value;
+         n.key = key;
+         cache.put(key, n);
+         if (head == null) {
+            head = n;
+            tail = n;
+         } else {
+            tail.next = n;
+            n.prev = tail;
+            tail = n;
+         }
+      }
+      if (cache.size() > capacity) {
+         cache.remove(head.key);
+         head = head.next;
+         head.prev = null;
+      }
+   }
+
+   public static void main(String[] args) {
+      LruCache lruCache = new LruCache(3);
+      lruCache.set(1,1);
+      lruCache.set(2,2);
+      lruCache.set(3,3);
+      lruCache.set(4,4);
+      assert 4 == lruCache.get(4);
+      assert 3 == lruCache.get(3);
+      assert 2 == lruCache.get(2);
+      assert -1 == lruCache.get(1);
+      lruCache.set(5,5);
+      assert -1 == lruCache.get(1);
+      assert 2 == lruCache.get(2);
+      assert 3 == lruCache.get(3);
+      assert -1 == lruCache.get(4);
+   }
+}
