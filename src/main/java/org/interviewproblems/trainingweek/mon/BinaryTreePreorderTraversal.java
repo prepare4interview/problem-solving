8c4e5bc952a2200cca0ff429f046e3b4fe2665d0
Prepare4Interview <prepare4interview@gmail.org>
2014-02-17 20:59:46 +0000

    enhdanced

--- src/main/java/org/interviewproblems/trainingweek/mon/BinaryTreePreorderTraversal.java
+++ src/main/java/org/interviewproblems/trainingweek/mon/BinaryTreePreorderTraversal.java
@@ -1,32 +1,51 @@
 package org.interviewproblems.trainingweek.mon;
 
-import org.interviewproblems.common.TreeNode;
+import java.util.HashMap;
+import java.util.Map;
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.LinkedList;
-
+//http://oj.leetcode.com/problems/copy-list-with-random-pointer/
 public class BinaryTreePreorderTraversal {
 
-   public class Solution {
+     class RandomListNode {
+         int label;
+         RandomListNode next, random;
+         RandomListNode(int x) { this.label = x; }
+     }
 
-      public ArrayList<Integer> preorderTraversal(TreeNode root) {
 
-         Deque<TreeNode> stack = new LinkedList<TreeNode>();
-         ArrayList<Integer> result = new ArrayList<Integer>();
+   public RandomListNode copyRandomList(RandomListNode head) {
+      if (head == null) return null;
+      RandomListNode clone = new RandomListNode(head.label);
+      RandomListNode it = head.next, cloneIt = clone;
+      Map<RandomListNode, Integer> node2index = new HashMap<RandomListNode, Integer>();
+      Map<Integer, RandomListNode> index2node = new HashMap<Integer, RandomListNode>();
 
-         if (root == null) return result;
+      node2index.put(head, 0);
+      index2node.put(0, clone);
 
-         stack.push(root);
-
-         while (!stack.isEmpty()) {
-            TreeNode n = stack.pop();
-            result.add(n.val);
-            if (n.right != null) stack.push(n.right);
-            if (n.left != null) stack.push(n.left);
-
-         }
-         return result;
+      int index = 1;
+      while (it != null) {
+         node2index.put(it, index);
+         cloneIt.next = new RandomListNode(it.label);
+         index2node.put(index++, cloneIt.next);
+         it = it.next;
+         cloneIt = cloneIt.next;
+         index2node.put(0, clone);
       }
+
+      it = head;
+      cloneIt = clone;
+      while (it != null) {
+         RandomListNode rnd = it.random;
+         cloneIt.random = index2node.get(node2index.get(rnd));
+         it = it.next;
+         cloneIt = cloneIt.next;
+      }
+
+      return clone;
+   }
+
+   public static void main(String[] args) {
+
    }
 }
