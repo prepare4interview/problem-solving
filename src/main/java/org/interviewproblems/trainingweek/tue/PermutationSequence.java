f484c905d9c0c4f005c41618262bbe759eb3de74
Prepare4Interview <prepare4interview@gmail.org>
2014-02-18 20:59:59 +0000

    Addes single number ii

--- src/main/java/org/interviewproblems/trainingweek/tue/PermutationSequence.java
+++ src/main/java/org/interviewproblems/trainingweek/tue/PermutationSequence.java
@@ -1,21 +1,41 @@
 package org.interviewproblems.trainingweek.tue;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class PermutationSequence {
 
    public String getPermutation(int n, int k) {
-      int[] perms = new int[n];
-      return getPermutations(n, k, 0, perms);
+      // int[] perms = new int[n];
+      ArrayList<Integer> elements = new ArrayList<Integer>();
+      for (int i = 1; i <= n; i++) elements.add(i);
+      getPermutation2(n, k, elements);
+      return result;
+   }
+
+   String result = "";
+   public void getPermutation2(int n, int k, List<Integer> remaining) {
+      if (remaining.size() == 1) {
+         result += remaining.remove(0);
+         return;
+      }
+      int window = 1;
+      for (int i = 1; i < remaining.size(); i++) window *= i;
+      int pos = (k-1) / window;
+      result += remaining.remove(pos);
+      getPermutation2(n-1, k - window, remaining);
    }
 
    int count = 0;
+   Set<Integer> currentPerm = new HashSet<Integer>();
 
    public String getPermutations(int n, int k, int pos, int[] perms) {
       if (pos == n) {
          int sum = 0;
          for (int i : perms) sum += i;
-         if (sum == (n * (n + 1))/2) {
-            count ++;
-         }
+         if (sum == (n * (n + 1))/2) count ++;
          if (count == k) {
             StringBuilder result = new StringBuilder();
             for (int i : perms) result.append(i);
@@ -24,19 +44,13 @@ public class PermutationSequence {
          return null;
       }
       for (int i = 1; i <= n; i++) {
+         if (!currentPerm.add(i)) continue;
          perms[pos] = i;
          String sol = getPermutations(n, k, pos + 1, perms);
          if (sol != null)
             return sol;
+         currentPerm.remove(i);
       }
       return null;
-   }
-
-   public static void main(String[] args) {
-      PermutationSequence ps = new PermutationSequence();
-      long duration = System.currentTimeMillis();
-      String permutation = ps.getPermutation(9, 273815);
-      System.out.println("took = " + (System.currentTimeMillis() - duration));
-      System.out.println("permutation = " + permutation);
    }
 }
